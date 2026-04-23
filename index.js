fb448168e6b45e6f0fc60d6c1257bec268de9267
dinhhaor151-arch <dinhhaor151@gmail.com>
2026-04-23 19:25:33 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 'use strict';
 const express = require('express');
 const fetch = require('node-fetch');
+const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 // ==================== CONFIG ====================
 const GAMES = {
